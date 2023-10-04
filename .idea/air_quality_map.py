7fc1afb273cf33e41cba9a7b7dc11d8f1e3af673
Alphie ochieng <24778809+alphieoch@users.noreply.github.com>
2023-10-04 23:40:50 +0100

    Update air_quality_map.py
    allow for use location to be Found Through IP

--- .idea/air_quality_map.py
+++ .idea/air_quality_map.py
@@ -2,11 +2,15 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import webbrowser
 import requests
-import pandas as pd
 import folium
-import os
 import platform
 import subprocess
+import warnings
+import geocoder
+from urllib3.exceptions import InsecureRequestWarning
+
+# Suppress SSL warnings
+warnings.simplefilter('ignore', InsecureRequestWarning)
 
 # Function to detect the system's theme (dark/light mode)
 def get_system_theme():
@@ -32,60 +36,83 @@ def get_system_theme():
             return 'light'
     return 'light'
 
-# Mocked IQAir request for demonstration purposes
-def mock_iqair_request():
-    return {
-        "status": "success",
-        "data": {
-            "city": "London",
-            "state": "England",
-            "country": "UK",
-            "location": {"type": "Point", "coordinates": [-0.1276474, 51.5072955]},
-            "current": {
-                "weather": {
-                    "ts": "2023-10-04T11:00:00.000Z",
-                    "tp": 13,
-                    "pr": 1017,
-                    "hu": 87,
-                    "ws": 1.5,
-                    "wd": 90,
-                    "ic": "10d"
-                },
-                "pollution": {
-                    "ts": "2023-10-04T10:00:00.000Z",
-                    "aqius": 58,
-                    "mainus": "p2",
-                    "aqicn": 25,
-                    "maincn": "p2"
-                }
-            }
-        }
-    }
-
-# Function to fetch air quality data from IQAir and update the GUI
-def fetch_air_quality_iqair():
-    response_data = mock_iqair_request()
-    air_quality_index = response_data["data"]["current"]["pollution"]["aqius"]
-    label_result.config(text=f"Air quality index for London is {air_quality_index}.")
-    m = folium.Map(location=[51.5074, -0.1278], zoom_start=10)
-    folium.Marker(location=[51.5074, -0.1278], popup=f'London: {air_quality_index} AQI').add_to(m)
-    m.save('air_quality_map.html')
-
-# Function to provide recommendations based on air quality value
-def show_recommendation():
+def fetch_current_location():
     try:
-        aqi = int(label_result.cget("text").split()[-1])
-        if aqi <= 50:
-            recommendation = "Air quality is good. No precautions necessary."
-        elif 50 < aqi <= 100:
-            recommendation = "Air quality is moderate. If you are particularly sensitive, consider staying indoors."
-        else:
-            recommendation = "Air quality is poor. Consider staying indoors and using an air purifier."
-        label_recommendation.config(text=recommendation)
+        location = geocoder.ip('me')
+        city = location.city
+        region = location.state
+        country = location.country
+        address_entry.delete(0, tk.END)
+        address_entry.insert(0, f"{city}, {region}, {country}")
+        fetch_air_quality_iqair()
     except:
-        messagebox.showerror("Error", "Fetch the air quality index first.")
+        messagebox.showerror("Error", "Failed to fetch your current location. Please check your internet connection and try again.")
 
-# Function to open the generated Folium map in the default web browser
+def fetch_air_quality_iqair():
+    address = address_entry.get()
+    api_url = f"https://api.iqair.com/v1/cities/{address}"
+    api_key = "c58f03e3-9d50-47b7-9306-08808afda89c"
+    response = requests.get(api_url, headers={"Authorization": f"Bearer {api_key}"}, verify=False)
+    if response.status_code == 200:
+        response_data = response.json()
+        air_quality_index = response_data["data"]["current"]["pollution"]["aqius"]
+        city_name = response_data["data"]["city"]
+        label_result.config(text=f"Air quality index for {city_name} is {air_quality_index}.")
+
+        # Mask rating logic
+        if air_quality_index <= 50:
+            mask_rating = 1
+        elif 50 < air_quality_index <= 100:
+            mask_rating = 3
+        elif 100 < air_quality_index <= 150:
+            mask_rating = 5
+        elif 150 < air_quality_index <= 200:
+            mask_rating = 7
+        elif 200 < air_quality_index <= 300:
+            mask_rating = 9
+        else:
+            mask_rating = 10
+        label_mask_rating.config(text=f"Face Mask Rating: {mask_rating}/10")
+
+        lat = response_data["data"]["location"]["coordinates"][1]
+        lon = response_data["data"]["location"]["coordinates"][0]
+        m = folium.Map(location=[lat, lon], zoom_start=10)
+        folium.Marker(location=[lat, lon], popup=f'{city_name}: {air_quality_index} AQI').add_to(m)
+        m.save('air_quality_map.html')
+    else:
+        messagebox.showerror("Error", "Failed to fetch air quality data. Please try again.")
+
+def display_help():
+    help_message = """
+Air Quality Checker Help:
+
+1. Enter your address (City, State, Country) in the provided textbox.
+2. Press 'Check Air Quality' to fetch the air quality index for that address.
+3. Alternatively, press 'Use Current Location' to automatically detect your location and fetch the air quality.
+4. The program will display the Air Quality Index (AQI) and a Face Mask Rating (on a 1-10 scale).
+    - Rating 1: Air quality is good. No need for a mask.
+    - Rating 10: Air quality is hazardous. It's highly recommended to wear a mask.
+5. Press 'Open Air Quality Map' to view the map with your location and AQI information.
+6. For recommendations on precautions, press 'Recommendations'.
+
+Remember, the air quality data is fetched from IQAir and may vary in real-time.
+    """
+    messagebox.showinfo("Help", help_message)
+
+def display_about():
+    about_message = """
+Air Quality Checker:
+
+This application fetches real-time air quality data from IQAir, a leading global air quality monitoring and data platform.
+
+The Face Mask Rating provided is a simple guide based on the Air Quality Index (AQI) to help users determine the necessity of wearing a mask. A rating closer to 10 indicates a higher recommendation to wear a mask.
+
+All data and recommendations are for informational purposes only. Always refer to local guidelines and health recommendations.
+
+All Rights Reserved. © 2023 Alphonce Ochieng
+    """
+    messagebox.showinfo("About", about_message)
+
 def open_map():
     webbrowser.open('air_quality_map.html')
 
@@ -104,32 +131,47 @@ else:
     entry_bg = '#FFFFFF'
     entry_fg = '#000000'
 
-# GUI building and event handling
+# Create a style object for ttk
+style = ttk.Style()
+style.configure('Custom.TLabel', background=bg_color, foreground=text_color)
+
+# GUI building
 window = tk.Tk()
 window.title("Air Quality Checker")
 window.geometry("600x400")
 window.config(bg=bg_color)
+
 frame_address = ttk.LabelFrame(window, text="Enter your address")
 frame_address.grid(row=0, column=0, pady=20, padx=20, sticky='nsew')
-address_entry = ttk.Entry(frame_address, width=40, background=entry_bg, foreground=entry_fg)
+
+address_entry = ttk.Entry(frame_address, width=40)
 address_entry.grid(row=0, column=0, pady=10, padx=10, sticky='nsew')
 address_entry.insert(0, "City, State, Country...")
+
 btn_fetch = ttk.Button(frame_address, text="Check Air Quality", command=fetch_air_quality_iqair)
 btn_fetch.grid(row=1, column=0, pady=10, sticky='nsew')
-label_result = ttk.Label(window, text="Air quality result will be displayed here.", bg=bg_color, fg=text_color)
-label_result.grid(row=1, column=0, pady=20, sticky='nsew')
-btn_map = ttk.Button(window, text="Open Air Quality Map", command=open_map)
-btn_map.grid(row=2, column=0, pady=10, sticky='nsew')
-label_recommendation = ttk.Label(window, text="", bg=bg_color, fg=text_color)
-label_recommendation.grid(row=3, column=0, pady=20, sticky='nsew')
-btn_recommendation = ttk.Button(window, text="Get Recommendation", command=show_recommendation)
-btn_recommendation.grid(row=4, column=0, pady=10, sticky='nsew')
 
-# Start the GUI's main loop (only if the script is run as the main module)
-if __name__ == "__main__":
-    window.mainloop()
+btn_current_location = ttk.Button(frame_address, text="Use Current Location", command=fetch_current_location)
+btn_current_location.grid(row=2, column=0, pady=10, sticky='nsew')
+
+label_result = ttk.Label(window, text="Air quality result will be displayed here.", style='Custom.TLabel')
+label_result.grid(row=1, column=0, pady=20, sticky='nsew')
+
+label_mask_rating = ttk.Label(window, text="Face Mask Rating will be displayed here.", style='Custom.TLabel')
+label_mask_rating.grid(row=2, column=0, pady=10, sticky='nsew')
+
+btn_map = ttk.Button(window, text="Open Air Quality Map", command=open_map)
+btn_map.grid(row=3, column=0, pady=10, sticky='nsew')
+
+btn_recommendations = ttk.Button(window, text="Recommendations", command=display_help)  # Reuse the help function here
+btn_recommendations.grid(row=4, column=0, pady=10, sticky='nsew')
+
+btn_about = ttk.Button(window, text="About", command=display_about)
+btn_about.grid(row=5, column=0, pady=10, sticky='nsew')
+
+btn_help = ttk.Button(window, text="?", command=display_help, width=2)
+btn_help.grid(row=0, column=1, pady=10, sticky='nsew')
+
+window.mainloop()
 
 # All Rights Reserved. © 2023 Alphonce Ochieng
-"""
-
-annotated_script_content
